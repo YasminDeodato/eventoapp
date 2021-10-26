e41f335c60951f6779ddd1a493d35b3779f19e2e
Yasmin Deodato <yasmin.deodato.beatriz@gmail.com>
2021-10-26 11:37:31 -0300

    Evento Update
    Criação do Evento Model, com atributos nome, data, local, horario e duracao. Sua respectiva página de cadastro também foi criada.

--- src/main/java/com/eventoapp/controllers/IndexController.java
+++ src/main/java/com/eventoapp/controllers/IndexController.java
@@ -1,10 +1,10 @@
-package com.eventoapp.eventoapp;
+package com.eventoapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexCcontroller {
+public class IndexController {
 	
 	@RequestMapping("/")
 	public String index() {
