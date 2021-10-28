8511c58955f470269349335144ecf931ae4ba839
Yasmin Deodato <yasmin.deodato.beatriz@gmail.com>
2021-10-28 11:02:00 -0300

    Configuração do Banco de Dados

---
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.eventoapp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import com.mysql.cj.jdbc.Driver;
+
+@Configuration
+public class DataConfiguration {
+	//Loading class `com.mysql.jdbc.Driver'. This is deprecated. 
+	//The new driver class is `com.mysql.cj.jdbc.Driver'. The driver is automatically registered via the SPI and manual loading of the driver class is generally unnecessary.
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/eventosapp");
+		dataSource.setUsername("root");
+		dataSource.setPassword("yasmin311");
+		return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,14 +1,31 @@
 package com.eventoapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventoapp.repository.EventoRepository;
+import com.eventoapp.models.Evento;
 
 @Controller
 public class EventoController {
 	
+	@Autowired
+	private EventoRepository eventoRepository;
+	
 	//formulario de cadastro de evento
-	@RequestMapping("/cadastrarEvento")
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";		
 	}
+	
+	//salvar dados do cadastro de evento no banco
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		eventoRepository.save(evento);
+		return "redirect:/cadastrarEvento";		
+	}
+	
+	
 }
